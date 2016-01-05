5f4673a480c1d65ded7eb3cbca65542f25634776
Ruben Barros <ruben.23@live.com.pt>
2016-01-05 15:58:18 +0000

    area_beacon filter by beacon

--- server/sga/rest/area_beacon.py
+++ server/sga/rest/area_beacon.py
@@ -5,6 +5,19 @@ from rest_framework import viewsets, permissions, serializers
 from sga.models import AreaBeacon
 
 
+class AreaBeaconFilter(django_filters.FilterSet):
+    beacon = django_filters.MethodFilter(action='beacon_filter')
+
+    class Meta:
+        model = AreaBeacon
+        fields = ['beacon']
+
+    def beacon_filter(self, queryset, value):
+        return queryset.filter(
+            beacon__uuid=value
+        )
+
+
 class AreaBeaconSerializer(serializers.ModelSerializer):
     class Meta:
         model = AreaBeacon
@@ -18,7 +31,7 @@ class AreaBeaconReadSerializer(serializers.ModelSerializer):
 
 class AreaBeaconViewSet(viewsets.ModelViewSet):
     model = AreaBeacon
-    filter_fields = ['beacon']
+    filter_class = AreaBeaconFilter
     queryset = AreaBeacon.objects.all()
 
     def get_serializer_class(self):
